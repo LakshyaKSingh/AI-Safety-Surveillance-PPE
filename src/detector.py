7f626c127ac49f92e41f3f7a6b195a45c4818ad7
Lakshya <lakshyasingh0806@gmail.com>
2026-01-22 10:58:55 +0530

    PPE Added

--- src/detector.py
+++ src/detector.py
@@ -2,8 +2,15 @@ from ultralytics import YOLO
 
 class PersonDetector:
     def __init__(self):
-        self.model = YOLO("yolov8n.pt")  # pretrained COCO
+        self.model = YOLO("yolov8n.pt")
 
-    def detect(self, frame):
-        results = self.model(frame, classes=[0], conf=0.4)
+    def track(self, frame):
+        results = self.model.track(
+            frame,
+            persist=True,
+            classes=[0],   # person only
+            conf=0.4,
+            iou=0.5,
+            tracker="bytetrack.yaml"
+        )
         return results[0]
--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
 import cv2
 from detector import PersonDetector
-from tracker import SimpleTracker
 from violation import ViolationTracker
 
 video_path = "../data/videos/input.mp4"
 
 cap = cv2.VideoCapture(video_path)
+if not cap.isOpened():
+    print("ERROR: Video not opened")
+    exit()
 
 detector = PersonDetector()
-tracker = SimpleTracker()
 violations = ViolationTracker()
 
 while cap.isOpened():
@@ -16,20 +17,32 @@ while cap.isOpened():
     if not ret:
         break
 
-    detections = detector.detect(frame)
-    tracked_people = tracker.update(detections)
+    results = detector.track(frame)
 
-    for track_id, x1, y1, x2, y2 in tracked_people:
-        duration = violations.update(track_id, missing_ppe=True)
+    if results.boxes.id is not None:
+        boxes = results.boxes.xyxy.cpu().numpy()
+        ids = results.boxes.id.cpu().numpy()
 
-        color = (0, 0, 255) if duration > 3 else (0, 255, 0)
-        cv2.rectangle(frame, (x1,y1), (x2,y2), color, 2)
-        cv2.putText(frame, f"ID:{track_id} {int(duration)}s",
-                    (x1, y1-10),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.6, color, 2)
+        for box, track_id in zip(boxes, ids):
+            x1, y1, x2, y2 = map(int, box)
 
-    cv2.imshow("Safety Surveillance", frame)
+            # simulate missing PPE for now
+            duration = violations.update(int(track_id), missing_ppe=True)
+
+            color = (0, 0, 255) if duration > 5 else (0, 255, 0)
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+            cv2.putText(
+                frame,
+                f"ID {int(track_id)} | {int(duration)}s",
+                (x1, y1 - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.6,
+                color,
+                2
+            )
+
+    cv2.imshow("Safety Surveillance - Tracking", frame)
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
